5c02b1da9e3510068154aaaaee34b112233afcde
Andi <andi.griesser@googlemail.com>
2020-05-04 00:32:17 +0200

    some refactoring and implemented planned Idle/Walk activity

--- VishyalEcoSim/ecosim.py
+++ VishyalEcoSim/ecosim.py
@@ -20,38 +20,41 @@ class Unit:
     self._currentProduction = list()
 
 
+  def getActivityConsumption(self, bPower, eff): #production cost calculation
+    if eff == 0:
+      return (0, 0)
+    time = self._buildTime/(bPower* eff)
+    mPTick = self._mCost / time
+    ePTick = self._eCost / time
+    return (mPTick, ePTick)
+
+
+  def progress(self, bPower, eff):
+    self._progress+=bPower*eff
+
+
   def getConsumption(self,mLimit,eLimit):
     mProd = -eLimit*self._mProd
     eProd = -self._eProd if self._eProd > 0 else -self._eProd*eLimit
+    eff = min([mLimit,eLimit])
   
     if not self._currentProduction:
       return (mProd, eProd)
 
-    if type(self._currentProduction[0]) == Reclaim:
-      time = self._currentProduction[0]._buildTime/self._bPower
-      mPTick = -self._currentProduction[0]._mCost / time
-      ePTick = -self._currentProduction[0]._eCost / time
-      return (mPTick+mProd, ePTick+eProd)
-
-    if min([mLimit,eLimit]) == 0:
-      return (mProd, eProd)
-
-    eff = min([mLimit,eLimit])
-    time = self._currentProduction[0]._buildTime/(self._bPower*eff)
-    mPTick = self._currentProduction[0]._mCost / time
-    ePTick = self._currentProduction[0]._eCost / time
+    mPTick, ePTick = self._currentProduction[0].getActivityConsumption(self._bPower,eff)
     return (mPTick+mProd, ePTick+eProd)
+
 
   
   def tick(self,mLimit,eLimit, eco):
     assert self._progress >= self._buildTime, self._name+f" ticked at {self._progress} of {self._buildTime}"
     m,e = self.getConsumption(mLimit,eLimit)
+    eff = min([mLimit,eLimit]) 
     if self._currentProduction:
-      eff = min([mLimit,eLimit]) if type(self._currentProduction[0]) == Unit else 1.0
-      self._currentProduction[0]._progress += self._bPower*eff
+      self._currentProduction[0].progress(self._bPower,eff)
 
       if self._currentProduction[0]._progress >= self._currentProduction[0]._buildTime:
-        print(self._currentProduction[0]._name,"done at",eco._time)
+        print(self._currentProduction[0]._name,"done at",eco._time,"by",self._name)
         if type(self._currentProduction[0]) == Unit:
           eco._units.append(self._currentProduction.pop(0))
         else:
@@ -67,6 +70,33 @@ class Reclaim:
     self._eCost = energy
     self._buildTime = buildTime
     self._progress = 0
+
+
+  def getActivityConsumption(self, bPower, eff): #reclaim gain calculation
+    time = self._buildTime/bPower
+    mPTick = -self._mCost / time
+    ePTick = -self._eCost / time
+    return (mPTick, ePTick)
+
+
+  def progress(self, bPower, eff):
+    self._progress+=bPower
+
+
+
+class Idle:
+  def __init__(self,time):
+    self._name = f"Wait/Walk {time}s"
+    self._buildTime = time
+    self._progress = 0
+
+  def progress(self, bPower, eff):
+    self._progress+=1
+
+  def getActivityConsumption(self, bPower, eff): #well
+    return (0, 0)
+
+
 
 class Eco:
   def __init__(self):
@@ -174,19 +204,31 @@ game._units[0]._currentProduction.append(copy.deepcopy(mex))
 game._units[0]._currentProduction.append(copy.deepcopy(mex))
 #game._units[0]._currentProduction.append(copy.deepcopy(pgen))
 
+fac._currentProduction[0]._currentProduction.append(Idle(5))
 fac._currentProduction[0]._currentProduction.append(copy.deepcopy(mex))
 #fac._currentProduction[0]._currentProduction.append(copy.deepcopy(pgen))
+fac._currentProduction[0]._currentProduction.append(Idle(1))
 fac._currentProduction[0]._currentProduction.append(copy.deepcopy(tree))
+fac._currentProduction[0]._currentProduction.append(Idle(1))
 fac._currentProduction[0]._currentProduction.append(copy.deepcopy(tree))
+fac._currentProduction[0]._currentProduction.append(Idle(1))
 fac._currentProduction[0]._currentProduction.append(copy.deepcopy(tree))
+fac._currentProduction[0]._currentProduction.append(Idle(1))
 fac._currentProduction[0]._currentProduction.append(copy.deepcopy(tree))
+fac._currentProduction[0]._currentProduction.append(Idle(1))
 fac._currentProduction[0]._currentProduction.append(copy.deepcopy(tree))
+fac._currentProduction[0]._currentProduction.append(Idle(1))
 fac._currentProduction[0]._currentProduction.append(copy.deepcopy(tree))
+fac._currentProduction[0]._currentProduction.append(Idle(1))
 fac._currentProduction[0]._currentProduction.append(copy.deepcopy(tree))
 
+fac._currentProduction[1]._currentProduction.append(Idle(5))
 fac._currentProduction[1]._currentProduction.append(copy.deepcopy(mex))
+fac._currentProduction[1]._currentProduction.append(Idle(1))
 fac._currentProduction[1]._currentProduction.append(copy.deepcopy(pgen))
+fac._currentProduction[1]._currentProduction.append(Idle(1))
 fac._currentProduction[1]._currentProduction.append(copy.deepcopy(pgen))
+fac._currentProduction[1]._currentProduction.append(Idle(1))
 fac._currentProduction[1]._currentProduction.append(copy.deepcopy(pgen))
 
 #for i in range(0,500):
