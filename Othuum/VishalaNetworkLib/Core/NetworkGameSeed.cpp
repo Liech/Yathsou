7a337077384d89e199304a616a53bf666f008d3f
Liech <nicolas_harttig@hotmail.de>
2020-06-30 00:13:26 +0200

    introduced targets in gameseed

--- Othuum/VishalaNetworkLib/Core/NetworkGameSeed.cpp
+++ Othuum/VishalaNetworkLib/Core/NetworkGameSeed.cpp
@@ -3,12 +3,31 @@
 namespace Vishala {
   NetworkGameSeed::NetworkGameSeed(NetworkGameSeedInput input) {
     _input = input;
+    initConnection();
+    initTargets();
+  }
+
+  void NetworkGameSeed::initTargets() {
+    _targets.clear();
+
+    for (auto target : _input.targets) {
+      Target t;
+      t.port = target.port;
+      t.ip   = target.IP;
+      t.id   = _targetIDCounter;
+      _targets.insert(t);
+      _targetIDCounter++;
+      _connection->connect(t.port, t.ip);
+    }
+  }
+
+  void NetworkGameSeed::initConnection(){
     _connection = std::make_unique<Connection>();
 
     _connection->setChannelCount(_input.numberOfChannels+1);
     _connection->setMaximumConnectionCount(512);
     _connection->setAcceptConnection(true);
-    _connection->setPort(input.ownPort);
+    _connection->setPort(_input.ownPort);
 
     _connection->setNewConnectionCallback([this](size_t clientnumber, std::string ip, int port) {
       newConnection(clientnumber, ip, port); 
@@ -40,6 +59,7 @@ namespace Vishala {
   }
 
   void NetworkGameSeed::connectionFailed(std::string name) {
+    
 
   }
 
--- Othuum/VishalaNetworkLib/Core/NetworkGameSeed.h
+++ Othuum/VishalaNetworkLib/Core/NetworkGameSeed.h
@@ -1,8 +1,10 @@
 #pragma once
 
-#include "NetworkGameSeedInput.h"
+
+#include <set>
 
 #include "Connection.h"
+#include "NetworkGameSeedInput.h"
 
 /*
   The GameSeeds goal is to get a list of ip+port & info packages and make sure that the seed is connected to all seeds on the other side
@@ -13,20 +15,34 @@
   Im not sure about the name. Something like "ConnectionManager" sounds lame. The primary job is to be the general core part of a startGame invocation
 */
 namespace Vishala {
-    class NetworkGameSeed {
-    public:
-      NetworkGameSeed(NetworkGameSeedInput input);
+  class NetworkGameSeed {
+  public:
+    NetworkGameSeed(NetworkGameSeedInput input);
 
-      void update();
+    void update();
 
-    private:
-      NetworkGameSeedInput        _input     ;
-      std::unique_ptr<Connection> _connection;
-
-      void newConnection        (size_t clientnumber, std::string ip, int port);
-      void connectionFailed     (std::string name);
-      void disconnect           (size_t clientnumber);
-      void messageRecived       (size_t channel, size_t clientNumber, std::unique_ptr<BinaryPackage>);
-      void controlMessageRecived(size_t clientNumber, std::unique_ptr<BinaryPackage>);
+  private:
+    struct Target {
+      std::string ip  ;
+      int         port;
+      size_t      id;
     };
+
+    std::set<Target>            _targets   ;
+    NetworkGameSeedInput        _input     ;
+    std::unique_ptr<Connection> _connection;
+    size_t                      _targetIDCounter = 0;
+
+    void initConnection();
+    void initTargets();
+
+    void newConnection        (size_t clientnumber, std::string ip, int port);
+    void connectionFailed     (std::string name);
+    void disconnect           (size_t clientnumber);
+    void messageRecived       (size_t channel, size_t clientNumber, std::unique_ptr<BinaryPackage>);
+    void controlMessageRecived(size_t clientNumber, std::unique_ptr<BinaryPackage>);
+
+
+  };
+
 }
