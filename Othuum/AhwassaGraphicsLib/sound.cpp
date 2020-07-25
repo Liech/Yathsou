0cab696ffafc07001a8d25b14340e09b8a675451
Liech <nicolas_harttig@hotmail.de>
2020-07-25 21:33:34 +0200

    integrated nyquistaudio to load music/sound files

--- Othuum/AhwassaGraphicsLib/sound.cpp
+++ Othuum/AhwassaGraphicsLib/sound.cpp
@@ -1,84 +1,63 @@
 #include "sound.h"
 
+#include <filesystem>
 #include <math.h>
 #include "lib/portaudio/portaudio.h"
 
-#define NUM_SECONDS   (1)
-#define SAMPLE_RATE   (44100)
-#define FRAMES_PER_BUFFER  (64)
-
-#ifndef M_PI
-#define M_PI  (3.14159265)
-#endif
-#define TABLE_SIZE   (200)
-typedef struct
-{
-  float sine[TABLE_SIZE];
-  int left_phase;
-  int right_phase;
-  char message[20];
-}
-paTestData;
-
-
-/* This routine will be called by the PortAudio engine when audio is needed.
-** It may called at interrupt level on some machines so don't do anything
-** that could mess up the system like calling malloc() or free().
-*/
-static int patestCallback(const void* inputBuffer, void* outputBuffer,
-  unsigned long framesPerBuffer,
-  const PaStreamCallbackTimeInfo* timeInfo,
-  PaStreamCallbackFlags statusFlags,
-  void* userData)
-{
-  paTestData* data = (paTestData*)userData;
-  float* out = (float*)outputBuffer;
-  unsigned long i;
-
-  (void)timeInfo; /* Prevent unused variable warnings. */
-  (void)statusFlags;
-  (void)inputBuffer;
-
-  for (i = 0; i < framesPerBuffer; i++)
-  {
-    *out++ = data->sine[data->left_phase];  /* left */
-    *out++ = data->sine[data->right_phase];  /* right */
-    data->left_phase += 1;
-    if (data->left_phase >= TABLE_SIZE) data->left_phase -= TABLE_SIZE;
-    data->right_phase += 3; /* higher pitch so we can distinguish left and right. */
-    if (data->right_phase >= TABLE_SIZE) data->right_phase -= TABLE_SIZE;
-  }
-
-  return paContinue;
-}
-
-/*
- * This routine is called by portaudio when playback is done.
- */
-static void StreamFinished(void* userData)
-{
-  paTestData* data = (paTestData*)userData;
-  //printf("Stream Completed: %s\n", data->message);
-}
+#include "lib/libnyquist/Decoders.h"
+#include "lib/libnyquist/Encoders.h"
 
 namespace Ahwassa
 {
+
+  /* This routine will be called by the PortAudio engine when audio is needed.
+  ** It may called at interrupt level on some machines so don't do anything
+  ** that could mess up the system like calling malloc() or free().
+  */
+  int sound::patestCallback(const void* inputBuffer, void* outputBuffer,
+    unsigned long framesPerBuffer,
+    const PaStreamCallbackTimeInfo* timeInfo,
+    unsigned long statusFlags,
+    void* userData)
+  {
+    float* out = (float*)outputBuffer;
+    Ahwassa::sound* s = (Ahwassa::sound*)userData;
+
+    (void)timeInfo; /* Prevent unused variable warnings. */
+    (void)statusFlags;
+    (void)inputBuffer;
+
+    for (size_t i = 0; i < framesPerBuffer*s->_fileData->channelCount; i++)
+    {
+      *out++ = s->_fileData->samples[s->_position + i] * 0.9;
+    }
+    s->_position += framesPerBuffer * s->_fileData->channelCount;
+    return paContinue;
+  }
+
+  /*
+   * This routine is called by portaudio when playback is done.
+   */
+  static void StreamFinished(void* userData)
+  {
+    //paTestData* data = (paTestData*)userData;
+    //printf("Stream Completed: %s\n", data->message);
+  }
+
+
   void sound::play()
   {
+    nqr::NyquistIO loader;
+    auto memory = nqr::ReadFile("YolonaOssData/sounds/testsound.wav");
+    _fileData = std::make_shared<nqr::AudioData>();
+    loader.Load(_fileData.get(), "wav", memory.buffer); //also mp3 possible
+
+    int FRAMES_PER_BUFFER = 64;
+
     PaStreamParameters outputParameters;
     PaStream* stream;
     PaError err;
-    paTestData data;
     int i;
-
-//    printf("PortAudio Test: output sine wave. SR = %d, BufSize = %d\n", SAMPLE_RATE, FRAMES_PER_BUFFER);
-
-    /* initialise sinusoidal wavetable */
-    for (i = 0; i < TABLE_SIZE; i++)
-    {
-      data.sine[i] = (float)sin(((double)i / (double)TABLE_SIZE) * M_PI * 2.);
-    }
-    data.left_phase = data.right_phase = 0;
 
     err = Pa_Initialize();
     if (err != paNoError) goto error;
@@ -97,11 +76,11 @@ namespace Ahwassa
       &stream,
       NULL, /* no input */
       &outputParameters,
-      SAMPLE_RATE,
+      _fileData->sampleRate,
       FRAMES_PER_BUFFER,
       paClipOff,      /* we won't output out of range samples so don't bother clipping them */
       patestCallback,
-      &data);
+      this);
     if (err != paNoError) goto error;
 
     //sprintf(data.message, "No Message");
@@ -112,7 +91,7 @@ namespace Ahwassa
     if (err != paNoError) goto error;
 
     //printf("Play for %d seconds.\n", NUM_SECONDS);
-    Pa_Sleep(NUM_SECONDS * 1000);
+    Pa_Sleep(_fileData->lengthSeconds * 1000);
 
     err = Pa_StopStream(stream);
     if (err != paNoError) goto error;
--- Othuum/AhwassaGraphicsLib/sound.h
+++ Othuum/AhwassaGraphicsLib/sound.h
@@ -1,10 +1,28 @@
 #pragma once
 
+#include <memory>
+namespace nqr
+{
+  struct AudioData;
+}
+struct PaStreamCallbackTimeInfo;
+
 namespace Ahwassa
 {
   class sound
   {
   public:
     void play();
+
+  private:
+    size_t                          _position = 0;
+    size_t                          _numberOfChannels = 0;
+    std::shared_ptr<nqr::AudioData> _fileData;
+
+    static int patestCallback(const void* inputBuffer, void* outputBuffer,
+      unsigned long framesPerBuffer,
+      const PaStreamCallbackTimeInfo* timeInfo,
+      unsigned long statusFlags,
+      void* userData);
   };
 }
