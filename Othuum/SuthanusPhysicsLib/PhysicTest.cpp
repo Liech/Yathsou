9e8256e66c31b47f5625afa666f5b35ccd42192f
Liech <nicolas_harttig@hotmail.de>
2020-07-27 23:24:16 +0200

    added bullet physic library

--- Othuum/SuthanusPhysicsLib/PhysicTest.cpp
+++ Othuum/SuthanusPhysicsLib/PhysicTest.cpp
@@ -2,10 +2,19 @@
 
 #include <iostream>
 
+#include "lib/bullet/btBulletDynamicsCommon.h"
+
+
 namespace Suthanus
 {
   void PhysicTest::go()
   {
     std::cout << "Huhu" << std::endl;
+    //New variables
+    btBroadphaseInterface* _broadphase;
+    btDefaultCollisionConfiguration* _collisionConfiguration;
+    btCollisionDispatcher* _dispatcher;
+    btSequentialImpulseConstraintSolver* _solver;
+    btDiscreteDynamicsWorld* _world;
   }
 }
