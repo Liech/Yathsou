31b8e0e9a29a907bec30f8cc8d7e5f38c9f9398d
Liech <nicolas_harttig@hotmail.de>
2020-10-30 23:21:14 +0100

    cleanup of scripting library. Improved interface

--- Othuum/HaasScriptingLib/ScriptEngine.cpp
+++ Othuum/HaasScriptingLib/ScriptEngine.cpp
@@ -11,37 +11,15 @@ namespace Haas
   ScriptEngine::ScriptEngine()
   {
     _state = luaL_newstate();
-    _api = new ScriptEngineAPI(_state);
     luaL_openlibs(_state);
-    _state->___scriptEngine = _api;
   }
 
   ScriptEngine::~ScriptEngine()
   {
     lua_close(_state);
-    delete _api;
   }
 
-  void ScriptEngine::blubb()
-  {
-
-    // Execution of a lua string
-    luaL_dostring(_state, "print \"Yo dude\"");
-
-    // Load a string and then execute it.
-    luaL_loadstring(_state, "io.write(\"I'm here too\\n\")");
-    lua_pcall(_state, 0, LUA_MULTRET, 0);
-
-    // Load from a file and then execute
-    if (luaL_loadfile(_state, "test/hello.lua") == 0) {
-      // File loaded call it
-      lua_pcall(_state, 0, LUA_MULTRET, 0);
-    }
-
-    executeFile("add.lua");
-  }
-
-  void ScriptEngine::executeFile(std::string filename)
+  void ScriptEngine::executeFile(const std::string& filename)
   {
     if (luaL_loadfile(_state, filename.c_str()) == 0) {
       lua_pcall(_state, 0, LUA_MULTRET, 0);
@@ -50,126 +28,32 @@ namespace Haas
       throw std::runtime_error("File not found");
   }
 
-  int ScriptEngine::callAdd(std::string name, int a, int b)
-  {
-    int sum;
-    lua_getglobal(_state, name.c_str());
-    _api->resetNumberOfPushes();
-    _api->pushNumber(a);
-    _api->pushNumber(b);
-    lua_call(_state, _api->numberOfPushes(), 1);
-    sum = _api->getStackElementAsInt(-1);
-    lua_pop(_state, 1);
-    _api->resetNumberOfPushes();
-    return sum;
-  }
-
-  void ScriptEngine::registerAverage()
-  {    
-    std::shared_ptr<std::function<void(ScriptEngineAPI*)>> p = std::make_shared<std::function<void(ScriptEngineAPI*)>>([](ScriptEngineAPI* api) {
-      int n = api->getStackCount();
-      double sum = 0;
-      int i;
-      for (i = 1; i <= n; i++)
-      {
-        /* total the arguments */
-        sum += api->getStackElementAsInt(i);
-      }
-      api->pushNumber(sum / n);
-      api->pushNumber(sum);
-    });
-    registerFunction("average", p);
-    luaL_dofile(_state, "average.lua");
-  }
-
-  void ScriptEngine::registerFunction(std::string name, std::shared_ptr<std::function<void(ScriptEngineAPI*)>> call)
+  void ScriptEngine::registerFunction(const std::string& name, std::shared_ptr<std::function<nlohmann::json(const nlohmann::json&)>> call)
   {
     _registry.insert(call);
     auto fn = Iyathuum::lambdaCapture2functionPointer<int(lua_State*)>([&](lua_State* state)
       {
-        auto api = (ScriptEngineAPI*)state->___scriptEngine;
-        api->resetNumberOfPushes();
-        (*call.get())(api);
-        return api->numberOfPushes();
+        
+        nlohmann::json input ;        
+        nlohmann::json output;
+        toJson(input);
+        output = (*call.get())(input);
+        toTable(output);
+        return 1;
       });
     lua_register(_state, name.c_str(), fn);
   }
 
-  std::vector<any> ScriptEngine::callScript(std::string name, std::vector<any> inputArgs, std::vector<any::type> outputTypes)
+  nlohmann::json ScriptEngine::callScript(const std::string& name, const nlohmann::json& input)
   {
+    nlohmann::json result;
     lua_getglobal(_state, name.c_str());
-    _api->resetNumberOfPushes();
-    for (int i = 0; i < inputArgs.size(); i++)
-    {
-      switch (inputArgs[i].get_type()) {
-      case any::type::Int:
-        _api->pushNumber(inputArgs[i].get_int());
-        break;
-      case any::type::Float:
-        _api->pushNumber(inputArgs[i].get_float());
-        break;
-      case any::type::String:
-        throw std::runtime_error("not implemented");
-        break;
-      }
-    }
-    lua_call(_state, _api->numberOfPushes(), outputTypes.size());
-    std::vector<any> result;
-    for (int i = 0; i < outputTypes.size(); i++)
-    {
-      switch (outputTypes[i]) {
-      case any::type::Int:
-        result.push_back(_api->getStackElementAsInt(-((int)outputTypes.size()) + i));
-        break;
-      case any::type::Float:
-        throw std::runtime_error("not implemented");
-        break;
-      case any::type::String:
-        throw std::runtime_error("not implemented");
-        break;
-      }
-    }
-    lua_pop(_state, (int)outputTypes.size());
-    _api->resetNumberOfPushes();
+    toTable(input);
+    lua_call(_state, 1,1);
+    toJson(result);
+    lua_pop(_state, 1);
     return result;
   }
-
-  ScriptEngineAPI::ScriptEngineAPI(lua_State* state)
-  {
-    _state = state;
-  }
-
-  int ScriptEngineAPI::getStackCount()
-  {
-    return lua_gettop(_state);
-  }
-
-  int ScriptEngineAPI::getStackElementAsInt(int number)
-  {
-    return lua_tointeger(_state, number);
-  }
-
-  double ScriptEngineAPI::getStackElementAsDouble(int number)
-  {
-    return lua_tonumber(_state, number);
-  }
-
-  void ScriptEngineAPI::pushNumber(int number)
-  {
-    lua_pushnumber(_state, number);
-    _numberOfPushes++;
-  }
-
-  void ScriptEngineAPI::resetNumberOfPushes()
-  {
-    _numberOfPushes = 0;
-  }
-
-  int ScriptEngineAPI::numberOfPushes()
-  {
-    return _numberOfPushes;
-  }
-
 
   void ScriptEngine::dumpStack()
   {
@@ -362,7 +246,7 @@ namespace Haas
     }
   }
 
-  void ScriptEngine::setVar(std::string name, nlohmann::json value) {
+  void ScriptEngine::setVar(const std::string& name, const nlohmann::json& value) {
     std::cout << "t1" << std::endl;
     dumpStack();
     std::cout << "ASD" << value.dump(4) << std::endl;
@@ -373,7 +257,7 @@ namespace Haas
     lua_setglobal(_state, name.c_str());
   }
 
-  nlohmann::json ScriptEngine::getVar(std::string name)
+  nlohmann::json ScriptEngine::getVar(const std::string& name)
   {
     lua_getglobal(_state, name.c_str());
     printTop(0);
--- Othuum/HaasScriptingLib/ScriptEngine.h
+++ Othuum/HaasScriptingLib/ScriptEngine.h
@@ -9,67 +9,27 @@ struct lua_State;
 
 namespace Haas
 {
-  struct any {
-    enum class type { Int, Float, String };
-    any(int   e) { m_data.INT = e; m_type = any::type::Int; }
-    any(float e) { m_data.FLOAT = e; m_type = any::type::Float; }
-    any(char* e) { m_data.STRING = e; m_type = any::type::String; }
-    type get_type() const { return m_type; }
-    int get_int() const { return m_data.INT; }
-    float get_float() const { return m_data.FLOAT; }
-    char* get_string() const { return m_data.STRING; }
-  private:
-    type m_type;
-    union {
-      int   INT;
-      float FLOAT;
-      char* STRING;
-    } m_data;
-  };
-
-  class ScriptEngineAPI
-  {
-  public:
-    ScriptEngineAPI(lua_State*);
-    int  getStackCount();
-    int  getStackElementAsInt(int number);
-    double  getStackElementAsDouble(int number);
-    void pushNumber(int number);
-    void resetNumberOfPushes();
-    int  numberOfPushes();
-
-  private:
-    lua_State* _state;
-    int        _numberOfPushes = 0;
-  };
-
   class ScriptEngine
   {
   public:
     ScriptEngine();
     ~ScriptEngine();
-    void blubb();
 
-    void executeFile(std::string filename);
-    int callAdd(std::string name, int a, int b);
-    ScriptEngineAPI* api() {
-      return _api;
-    };
+    void           executeFile     (const std::string& filename                         );
+    nlohmann::json callScript      (const std::string& name, const nlohmann::json& input);
+    nlohmann::json getVar          (const std::string& name                             );
+    void           setVar          (const std::string& name, const nlohmann::json& value);
+    void           registerFunction(const std::string& name, std::shared_ptr < std::function < nlohmann::json(const nlohmann::json&)>>);
+  private:
     void dumpStack();
+    void printTop(int indentation = 0);
     void dumpGlobalVariables();
 
-    std::vector<any> callScript(std::string name, std::vector<any>, std::vector<any::type> outputTypes = {});
-    nlohmann::json getVar(std::string name);
-    void           setVar(std::string name, nlohmann::json value);
-    void registerAverage();
-    void registerFunction(std::string name, std::shared_ptr < std::function < void(ScriptEngineAPI*)>>);
-  private:
-    void toTable(const nlohmann::json& json);
-    void toJson(nlohmann::json& json);
-    std::string popStr(int pos);
-    void printTop(int indentation = 0);
-    std::set<std::shared_ptr<std::function<void(ScriptEngineAPI*)>>> _registry;
-    lua_State*       _state;
-    ScriptEngineAPI* _api;
+    void        toTable(const nlohmann::json& json);
+    void        toJson (nlohmann::json& json);
+    std::string popStr (int pos);
+
+    std::set<std::shared_ptr<std::function < nlohmann::json(const nlohmann::json&)>>> _registry;
+    lua_State*                                                                        _state;
   };
 }
