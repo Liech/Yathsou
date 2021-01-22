0d6f5ffeda788883a6c64ce851a33b535e3f034c
Nicolas Harttig <nicolas_harttig@hotmail.de>
2021-01-22 19:53:30 +0100

    timed update

--- Othuum/Fervor/main.cpp
+++ Othuum/Fervor/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "IyathuumCoreLib/Singleton/Database.h"
+#include "IyathuumCoreLib/Util/UpdateTimer.h"
 #include "YolonaOss/OpenGL/Window.h"
 #include "Fervor.h"
 
@@ -10,15 +11,15 @@ int main(int argc, char** argv) {
   int height =600;
   YolonaOss::GL::Window w(width, height);
   Fervor f;
-
+  Iyathuum::UpdateTimer timer([&f]() {f.update(); },30);
   bool init = false;
 
-  w.Update = [&f,&init,&w]() {
+  w.Update = [&f,&init,&w,&timer]() {
     if (!init) {
       init = true;
       f.load(w.getSpec());
     }
-    f.update();
+    timer.update();
     f.draw  ();
   };
   w.run();
