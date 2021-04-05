6f7fdb15ca36d9654cae1bccf6387f4a1916d9a3
Liech <nicolas_harttig@hotmail.de>
2021-04-05 18:33:43 +0200

    slider transfered

--- Othuum/AhwassaGraphicsLib/Widgets/Slider.h
+++ Othuum/AhwassaGraphicsLib/Widgets/Slider.h
@@ -1,51 +1,49 @@
-//#pragma once
-//
-//#include "Widget.h"
-//#include <string>
-//#include <functional>
-//
-//namespace YolonaOss {
-//  namespace Widgets {
-//    class Slider : public Widget
-//    {
-//    public:
-//      Slider();
-//      Slider(Iyathuum::AABB<2> position, double min, double max, double startValue, std::function<void(double)> valueChangedCall);
-//      ~Slider();
-//
-//      // Inherited via Drawable
-//      virtual void load(GL::DrawSpecification*) override;
-//      virtual void draw() override;
-//
-//      virtual void mouseEnter() override;
-//      virtual void mouseLeave() override;
-//      virtual void mouseMove(glm::vec2 position) override;
-//      virtual bool mouseStatusChanged(glm::vec2 position, Iyathuum::Key k, Iyathuum::KeyStatus status) override;
-//
-//      double getValue() { return _currentValue; }
-//      void   setValue(double value, bool emit = true);
-//      virtual void setVisible(bool visible) override;
-//
-//    private:
-//      Iyathuum::AABB<2> getSliderLocation();
-//      Iyathuum::AABB<2> getBarLocation();
-//
-//      const float leftPad      = 0.4f;
-//      const float rightPad     = 0.05f;
-//      const float barHeight    = 0.3f;
-//      const float sliderHeight = 0.6f;
-//      const float sliderWidth  = 0.1f;
-//      const float minSliderWidth = 15;
-//
-//
-//      std::function<void(double)> _valueChangedCall;
-//      double                      _min;
-//      double                      _max;
-//      double                      _currentValue;
-//
-//      bool _pressed = false;
-//
-//      bool _hovered = false;
-//    };
-//  }
-//}
+#pragma once
+
+#include <string>
+#include <functional>
+
+#include "AhwassaGraphicsLib/Input/UIElement.h"
+#include "AhwassaGraphicsLib/Drawables/Drawable.h"
+
+namespace Ahwassa {
+  class Slider : public UIElement, public Drawable
+  {
+  public:
+    Slider(Window*);
+    Slider(Iyathuum::glmAABB<2> position, double min, double max, double startValue, std::function<void(double)> valueChangedCall, Window* w, std::shared_ptr<UIElement> parent = nullptr);
+    ~Slider();
+
+    virtual void draw() override;
+
+    virtual void mouseEnterEvent() override;
+    virtual void mouseLeaveEvent() override;
+    virtual bool mouseMoveEvent(glm::vec2 current, glm::vec2 movement) override;
+    virtual bool mouseEvent(glm::vec2 localPosition, Iyathuum::Key button, Iyathuum::KeyStatus status) override;
+
+    double getValue() { return _currentValue; }
+    void   setValue(double value, bool emit = true);
+    virtual void setVisible(bool visible) override;
+
+  private:
+    Iyathuum::glmAABB<2> getSliderLocation();
+    Iyathuum::glmAABB<2> getBarLocation();
+
+    const float leftPad = 0.4f;
+    const float rightPad = 0.05f;
+    const float barHeight = 0.3f;
+    const float sliderHeight = 0.6f;
+    const float sliderWidth = 0.1f;
+    const float minSliderWidth = 15;
+
+
+    std::function<void(double)> _valueChangedCall;
+    double                      _min;
+    double                      _max;
+    double                      _currentValue;
+
+    bool _pressed = false;
+
+    bool _hovered = false;
+  };
+}
