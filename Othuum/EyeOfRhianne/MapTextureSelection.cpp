0de5d5b4105cefbe368a86be27b4a895e255f5cd
Nicolas Harttig <nicolas_harttig@hotmail.de>
2021-05-13 12:35:16 +0200

    additional textures from map file shown

--- Othuum/EyeOfRhianne/MapTextureSelection.cpp
+++ Othuum/EyeOfRhianne/MapTextureSelection.cpp
@@ -33,6 +33,18 @@ MapTextureSelection::MapTextureSelection(Iyathuum::glmAABB<2> area, Graphic& gra
   textures.push_back("Flatness");
   textures.push_back("Depth Bias Mask");
   textures.push_back("Terrain Type");
+  textures.push_back("High Red"  );
+  textures.push_back("High Green");
+  textures.push_back("High Blue" );
+  textures.push_back("High Alpha");
+  textures.push_back("Low Red"   );
+  textures.push_back("Low Green" );
+  textures.push_back("Low Blue"  );
+  textures.push_back("Low Alpha" );
+  textures.push_back("Water Red"   );
+  textures.push_back("Water Green" );
+  textures.push_back("Water Blue"  );
+  textures.push_back("Water Alpha" );
 
 
   _list = std::make_unique<ListSelection>(textures, textures, area, _graphic.getWindow(), [this](std::string img) {
@@ -56,6 +68,44 @@ void MapTextureSelection::setImage(std::string img) {
     _graphic._previewImage = std::make_shared<Ahwassa::Texture>("Preview", _graphic._currentMap->scmap().waterMapTexture.get());
   else if (img == "Normal")
     _graphic._previewImage = std::make_shared<Ahwassa::Texture>("Preview", _graphic._currentMap->scmap().normalMap.get());
+  else if (
+    img == "High Red"    ||
+    img == "High Green"  ||
+    img == "High Blue"   ||
+    img == "High Alpha"  ||
+    img == "Low Red"     ||
+    img == "Low Green"   ||
+    img == "Low Blue"    ||
+    img == "Low Alpha"   ||
+    img == "Water Red"   ||
+    img == "Water Green" ||
+    img == "Water Blue"  ||
+    img == "Water Alpha"
+    ) {
+    Iyathuum::MultiDimensionalArray<Iyathuum::Color, 2>* im;
+    int channel = 0;
+    if (img == "High Red") { channel = 0; im = _graphic._currentMap->scmap().highTexture.get(); }
+    else if (img == "High Green" ) { channel = 1; im = _graphic._currentMap->scmap().highTexture.get(); }
+    else if (img == "High Blue"  ) { channel = 2; im = _graphic._currentMap->scmap().highTexture.get(); }
+    else if (img == "High Alpha" ) { channel = 3; im = _graphic._currentMap->scmap().highTexture.get(); }
+    else if (img == "Low Red"    ) { channel = 0; im = _graphic._currentMap->scmap().lowTexture.get(); }
+    else if (img == "Low Green"  ) { channel = 1; im = _graphic._currentMap->scmap().lowTexture.get(); }
+    else if (img == "Low Blue"   ) { channel = 2; im = _graphic._currentMap->scmap().lowTexture.get(); }
+    else if (img == "Low Alpha"  ) { channel = 3; im = _graphic._currentMap->scmap().lowTexture.get(); }
+    else if (img == "Water Red"  ) { channel = 0; im = _graphic._currentMap->scmap().waterMapTexture.get(); }
+    else if (img == "Water Green") { channel = 1; im = _graphic._currentMap->scmap().waterMapTexture.get(); }
+    else if (img == "Water Blue" ) { channel = 2; im = _graphic._currentMap->scmap().waterMapTexture.get(); }
+    else if (img == "Water Alpha") { channel = 3; im = _graphic._currentMap->scmap().waterMapTexture.get(); }
+    else throw std::runtime_error("Unkown type");
+
+    std::unique_ptr<Iyathuum::MultiDimensionalArray<Iyathuum::Color, 2>> colored = std::make_unique< Iyathuum::MultiDimensionalArray<Iyathuum::Color, 2>>(im->getDimensionVector());
+    colored->apply([&im,channel](size_t pos, Iyathuum::Color& clr) {
+      auto d = im->get_linearVal(pos)[channel];
+      Iyathuum::Color result(d, 0, 0, 255);
+      clr = result;
+    });
+    _graphic._previewImage = std::make_shared<Ahwassa::Texture>("Preview", colored.get());
+  }
   else {
     Iyathuum::MultiDimensionalArray<unsigned char, 2>* m;
     if (img == "Water Foam Mask")
