ffaf9c500b0f4942ffeb65a5ad9ab95ce6b4abfc
Liech <nicolas_harttig@hotmail.de>
2021-07-11 01:56:23 +0200

    barebone out superb commander

--- Othuum/SuperbCommander/main.cpp
+++ Othuum/SuperbCommander/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <filesystem>
 
+#include "AhwassaGraphicsLib/Core/Window.h"
+#include "AhwassaGraphicsLib/Drawables/Background.h"
+#include "AhwassaGraphicsLib/Drawables/FPS.h"
+
+void enforceWorkingDir(std::string exeDir) {
+  const size_t last_slash_idx = exeDir.find_last_of("\\/");
+  if (std::string::npos != last_slash_idx)
+  {
+    exeDir.erase(last_slash_idx + 1);
+  }
+  std::filesystem::current_path(exeDir);
+}
 
 int main(int argc, char** argv) {
-  std::cout << "HUHU" << std::endl;
+  enforceWorkingDir(std::string(argv[0]));
+  int width = 800;
+  int height = 600;
+  Ahwassa::Window w(width, height);
+
+  Ahwassa::Background b(&w);
+  std::unique_ptr<Ahwassa::FPS> fps;
+
+
+  w.Startup = [&]() {
+    fps = std::make_unique<Ahwassa::FPS>(&w);
+
+  };
+
+  w.Update = [&]() {
+
+    b.draw();
+    fps->draw();
+  };
+  w.run();
+
 }
