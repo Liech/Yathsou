5c82047c7ff0915f0d9c36e5ef18a840e5a367b5
Liech <nicolas_harttig@hotmail.de>
2022-06-05 00:01:31 +0200

    pybind11 works

--- HaasScriptingLib/Source/HaasScriptingLib/PythonEngine.cpp
+++ HaasScriptingLib/Source/HaasScriptingLib/PythonEngine.cpp
@@ -1,13 +1,29 @@
 #include "PythonEngine.h"
 #include <python3.10/Python.h>
+#include <iostream>
+#include <pybind11/embed.h>
 
 namespace Haas {
+
+  void greetTheWorld() {
+    std::cout << "Hallo Welt" << std::endl;
+  }
+
   PythonEngine::PythonEngine() {
-    Py_SetProgramName(L"Haas");
+    //Py_SetProgramName(L"Haas");
     Py_SetPythonHome(L"Data/python");
-    Py_Initialize();
-    PyObject* sys = PyImport_ImportModule("sys");
-    PyRun_SimpleString("from time import time,ctime\n"
-      "print('Today is', ctime(time()))\n");
+    //Py_Initialize();
+
+    pybind11::scoped_interpreter guard{};
+
+    pybind11::print("Hello, World!");
+
+    pybind11::exec(R"(
+        kwargs = dict(name="World", number=42)
+        message = "Hello, {name}! The answer is {number}".format(**kwargs)
+        print(message)
+    )");
+
+
   }
 }
